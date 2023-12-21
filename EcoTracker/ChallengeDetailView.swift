894c13d82a51a997789f548b6d955586f873f7ba
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-12-21 14:23:29 +0800

    first commit in a long time, suggestion view complete and challenges view and detail views realted to suggestion view in progress

---
+++ EcoTracker/ChallengeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeDetailView.swift
+//  EcoTracker
+//
+//  Created by Venkatesh Devendran on 21/12/2023.
+//
+
+import SwiftUI
+
+struct ChallengeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengeDetailView()
+}
---
+++ EcoTracker/ChallengeView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeView.swift
+//  EcoTracker
+//
+//  Created by Venkatesh Devendran on 21/12/2023.
+//
+
+import SwiftUI
+
+struct ChallengeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengeView()
+}
