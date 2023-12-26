cb2f9b0f15facca7025f40334c24e7751bf18ef7
Vensah <83892875+vensah-dev@users.noreply.github.com>
2023-12-26 23:32:02 +0800

    profile view and persistance for it

---
+++ Ecopedia/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  EcoTracker
+//
+//  Created by Venkatesh Devendran on 26/12/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
